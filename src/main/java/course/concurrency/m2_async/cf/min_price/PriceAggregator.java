e6cc2f499ed16bd3abbc8087aa740f776a5f631b
k.kobyshev <k.kobyshev@qiwi.com>
2023-10-18 00:24:52 +0300

    get min price

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,7 +1,8 @@
 package course.concurrency.m2_async.cf.min_price;
 
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class PriceAggregator {
 
@@ -13,12 +14,28 @@ public class PriceAggregator {
 
     private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
+
+    private final Executor executor = Executors.newCachedThreadPool();
+
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
     }
 
     public double getMinPrice(long itemId) {
-        // place for your code
-        return 0;
+        var shopIdFutures = shopIds.stream()
+                .map(
+                        shopId -> CompletableFuture.supplyAsync(
+                                        () -> Optional.of(priceRetriever.getPrice(itemId, shopId)),
+                                        executor
+                                ).exceptionally(e -> Optional.empty())
+                                .completeOnTimeout(Optional.empty(), 2950, TimeUnit.MILLISECONDS)
+                ).collect(Collectors.toList());
+        return shopIdFutures
+                .stream()
+                .map(CompletableFuture::join)
+                .filter(Optional::isPresent)
+                .mapToDouble(Optional::get)
+                .min()
+                .orElse(Double.NaN);
     }
 }
